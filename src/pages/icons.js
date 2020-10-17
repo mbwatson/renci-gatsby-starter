4b0629b0e88bcd9dfcf17d7c8fc20112549b1be4
Matt Watson <mattwat@gmail.com>
2020-10-17 16:10:02 -0400

    update icons
    add icons

--- src/pages/icons.js
+++ src/pages/icons.js
@@ -7,12 +7,12 @@ import { Container as Grid, Row, Col } from 'react-grid-system'
 import { Title, Heading } from '../components/typography'
 import { Icon } from '../components/icon'
 
-const IconDemo = ({ icon, text }) => {
+const IconDemo = ({ icon }) => {
   const theme = useTheme()
   return (
     <Fragment>
       <Icon icon={ icon } size={ 36 } fill={ theme.color.primary.main } style={{ margin: '1rem' }} />
-      <div>{ text }</div>
+      <div>{ icon }</div>
     </Fragment>
   )
 }
@@ -27,51 +27,15 @@ export default () => {
       <Container>
         <Title>Icons</Title>
         
-        <Heading>Actions</Heading>
-        <Grid fluid>
-          <Row>
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="backspace" text="backspace" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="cancel" text="cancel" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="close" text="close" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="delete" text="delete" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="download" text="download" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="hamburger" text="hamburger" />
-            </Col>          
-            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="magnifyingGlass" text="magnifying glass" />
-            </Col>          
-          </Row>
-        </Grid>
-        <Pre>
-{`<Icon icon="backspace" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="cancel" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="close" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="delete" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="download" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="hamburger" size={ 36 } fill={ theme.color.primary.main } />
-<Icon icon="magnifyingGlass" size={ 36 } fill={ theme.color.primary.main } />`}
-        </Pre>
-
         <Heading>Account</Heading>
         <Grid fluid>
           <Row>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="user" text="user" />
-            </Col>          
+              <IconDemo icon="user" />
+            </Col>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="userCircle" text="userCircle" />
-            </Col>          
+              <IconDemo icon="userCircle" />
+            </Col>
           </Row>
         </Grid>
         <Pre>
@@ -79,21 +43,69 @@ export default () => {
 <Icon icon="userCircle" size={ 36 } fill={ theme.color.primary.main } />`}
         </Pre>
 
-        <Heading>Chevrons</Heading>
+        <Heading>Actions</Heading>
+        <Grid fluid>
+          <Row>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="backspace" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="cancel" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="close" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="copy" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="cut" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="delete" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="download" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="hamburger" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="magnifyingGlass" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="paste" />
+            </Col>
+          </Row>
+        </Grid>
+        <Pre>
+{`<Icon icon="backspace" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="cancel" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="close" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="copy" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="cut" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="delete" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="download" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="hamburger" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="magnifyingGlass" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="paste" size={ 36 } fill={ theme.color.primary.main } />`}
+        </Pre>
+
+        <Heading>Arrows</Heading>
         <Grid fluid>
           <Row>
             <Col xs={ 6 } sm={ 3 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="chevronUp" text="chevron up" />
-            </Col>          
+              <IconDemo icon="chevronUp" />
+            </Col>
             <Col xs={ 6 } sm={ 3 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="chevronDown" text="chevron down" />
-            </Col>          
+              <IconDemo icon="chevronDown" />
+            </Col>
             <Col xs={ 6 } sm={ 3 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="chevronLeft" text="chevron left" />
-            </Col>          
+              <IconDemo icon="chevronLeft" />
+            </Col>
             <Col xs={ 6 } sm={ 3 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="chevronRight" text="chevron right" />
-            </Col>          
+              <IconDemo icon="chevronRight" />
+            </Col>
           </Row>
         </Grid>
         <Pre>
@@ -107,14 +119,14 @@ export default () => {
         <Grid fluid>
           <Row>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="pause" text="pause" />
-            </Col>          
+              <IconDemo icon="pause" />
+            </Col>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="play" text="play" />
-            </Col>          
+              <IconDemo icon="play" />
+            </Col>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="stop" text="stop" />
-            </Col>          
+              <IconDemo icon="stop" />
+            </Col>
           </Row>
         </Grid>
         <Pre>
@@ -123,12 +135,70 @@ export default () => {
 <Icon icon="stop" size={ 36 } fill={ theme.color.primary.main } />`}
         </Pre>
 
+        <Heading>WWW</Heading>
+        <Grid fluid>
+          <Row>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="email" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="facebook" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="github" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="instagram" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="link" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="linkedIn" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="rss" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="slack" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="twitter" />
+            </Col>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="youtube" />
+            </Col>
+          </Row>
+        </Grid>
+        <Pre>
+{`<Icon icon="email" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="facebook" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="github" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="instagram" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="link" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="linkedIn" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="rss" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="slack" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="twitter" size={ 36 } fill={ theme.color.primary.main } />
+<Icon icon="youtube" size={ 36 } fill={ theme.color.primary.main } />`}
+        </Pre>
+
+        <Heading>RENCI</Heading>
+        <Grid fluid>
+          <Row>
+            <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
+              <IconDemo icon="renciDash" />
+            </Col>
+          </Row>
+        </Grid>
+        <Pre>{`<Icon icon="renciDash" size={ 36 } fill={ theme.color.primary.main } />`}</Pre>
+
         <Heading>Notification</Heading>
         <Grid fluid>
           <Row>
             <Col xs={ 4 } sm={ 3 } lg={ 2 } style={{ textAlign: 'center'}}>
-              <IconDemo icon="warning" text="warning" />
-            </Col>          
+              <IconDemo icon="warning" />
+            </Col>
           </Row>
         </Grid>
         <Pre>{`<Icon icon="warning" size={ 36 } fill={ theme.color.primary.main } />`}</Pre>
